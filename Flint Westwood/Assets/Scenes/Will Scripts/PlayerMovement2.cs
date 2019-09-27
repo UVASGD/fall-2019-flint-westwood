185f4c44a191635047db4f1b4c06485fad5b5afd
WHaslet <55633796+WHaslet@users.noreply.github.com>
2019-09-27 15:33:22 -0400

    Basic movement script

---
+++ Flint Westwood/Assets/Scenes/Will Scripts/PlayerMovement2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D myRB;
+
+    private void Update()
+    {
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        myRB.position += new Vector2(moveX, moveY) * Time.deltaTime * moveSpeed;
+        
+    }
+
+}
