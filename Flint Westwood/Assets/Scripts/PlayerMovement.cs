4cdf5cd23928e5eacdef26be31868121008cb583
Joshua Devine <joshuaddevine@gmail.com>
2019-10-19 15:20:40 -0400

    Update PlayerMovement.cs

--- Flint Westwood/Assets/Scripts/PlayerMovement.cs
+++ Flint Westwood/Assets/Scripts/PlayerMovement.cs
@@ -31,14 +31,20 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    	
     }
 
     // Update is called once per frame
     void Update()
     {
+    
+    }
+    
+    void FixedUpdate()
+    {
         ProcessInputs();
-        if (isSprinting)
+    
+    	if (isSprinting)
         {
             Run();
         }
