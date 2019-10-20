df7d9dfc55f5b95c3960c7d4a0a9ed1ba3ba57fb
hw-2030 <hydenw2030@gmail.com>
2019-10-20 16:23:13 -0400

    Consolidated player movement script and streamlined sprint functionality

--- Flint Westwood/Assets/Scripts/PlayerMovement.cs
+++ Flint Westwood/Assets/Scripts/PlayerMovement.cs
@@ -2,124 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Character stats:")]
-    public Vector2 movementDirection;
-    public float movementSpeed;
+    [SerializeField] private Vector2 movementDirection;
+    [SerializeField] private float movementSpeed;
 
     [Space]
     [Header("Character attributes:")]
-    public float WALK_BASE_SPEED = 1.0f;
-    public float SPRINT_BASE_SPEED = 1.5f;
-
-    [Space]
-    [Header("Sprint controls:")]
-    public float TAP_TIME = 0.5f;
-
-    [Space]
-    [Header("References")]
-    Animator playerAnim;
-
-    public Rigidbody2D rb;
+    [SerializeField] private float WALK_BASE_SPEED = 1.0f;
+    [SerializeField] private float SPRINT_BASE_SPEED = 1.5f;
     
-    private bool isSprinting = false;   //True if character currently sprinting
+    private Rigidbody2D rb;
+    private Vector3 playerMovement;
+    
+    [SerializeField] bool isSprinting = false;   //True if character currently sprinting
 
-    private float lastTapTime = -1.0f;  //for detecting double tap
-    private KeyCode lastKeyPressed = KeyCode.F;
-
-    // Start is called before the first frame update
     void Start()
     {
-    	
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-    
+        
     }
     
     void FixedUpdate()
     {
         ProcessInputs();
-    
-    	if (isSprinting)
-        {
-            Run();
-        }
-        else
-        {
-            Walk();
-        }
+        HandleMovement();
     }
 
     void ProcessInputs() 
     {
-        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f,1.0f);
-        movementDirection.Normalize();
-
-        if (Input.GetKeyDown(KeyCode.D))  //CHECKING FOR D DOUBLE TAP
+        playerMovement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+        playerMovement = playerMovement.normalized;
+        if (Input.GetButton("Sprint"))
         {
-            if(lastKeyPressed == KeyCode.D && Time.time - lastTapTime < TAP_TIME)
-            {
-                isSprinting = true;
-            }
-
-            lastKeyPressed = KeyCode.D;
-            lastTapTime = Time.time;
+            this.isSprinting = true;
+            Debug.Log("Sprinting!");
         }
-
-        if (Input.GetKeyDown(KeyCode.A))  //CHECKING FOR A DOUBLE TAP
+        else
         {
-            if (lastKeyPressed == KeyCode.A && Time.time - lastTapTime < TAP_TIME)
-            {
-                isSprinting = true;
-            }
-
-            lastKeyPressed = KeyCode.A;
-            lastTapTime = Time.time;
+            this.isSprinting = false;
         }
-
-        if (Input.GetKeyDown(KeyCode.S))  //CHECKING FOR S DOUBLE TAP
-        {
-            if (lastKeyPressed == KeyCode.S && Time.time - lastTapTime < TAP_TIME)
-            {
-                isSprinting = true;
-            }
-
-            lastKeyPressed = KeyCode.S;
-            lastTapTime = Time.time;
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))  //CHECKING FOR W DOUBLE TAP
-        {
-            if (lastKeyPressed == KeyCode.W && Time.time - lastTapTime < TAP_TIME)
-            {
-                isSprinting = true;
-            }
-
-            lastKeyPressed = KeyCode.W;
-            lastTapTime = Time.time;
-        }
-
-        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A))
-        {
-            isSprinting = false;
-        }
-
+        
     }
-
-    void Walk()
+    
+    void HandleMovement()
     {
-        rb = GetComponent<Rigidbody2D> ();
-        rb.velocity = movementDirection * movementSpeed * WALK_BASE_SPEED;
-    }
+        Vector3 playerVelocity;
 
-    void Run()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = movementDirection * movementSpeed * SPRINT_BASE_SPEED;
+        if (this.isSprinting)
+        {
+            playerVelocity = Time.deltaTime * movementSpeed * SPRINT_BASE_SPEED * playerMovement;
+        }
+        else
+        {
+            playerVelocity = Time.deltaTime * movementSpeed * WALK_BASE_SPEED * playerMovement;
+
+        }
+
+        rb.MovePosition(transform.position + playerVelocity);
     }
 }
