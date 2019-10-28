4165d015e62990c54830dc5d0d73e5f819f7fced
WHaslet <55633796+WHaslet@users.noreply.github.com>
2019-10-28 18:52:27 -0400

    Fixed Bullet Ricochet

---
+++ Flint Westwood/Assets/Scripts/FixedBulletRicochet/Scripts/BulletController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private int numberOfHits;
+    [SerializeField] private float bounceDistance;
+    [SerializeField] private float damage;
+
+    private Vector3 lastPosition;
+
+    private void Update()
+    {
+
+        transform.position += transform.up * moveSpeed * Time.deltaTime;
+
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, transform.up, bounceDistance);
+
+        if (rayHit)
+        {
+
+            Breakable health = rayHit.collider.GetComponent<Breakable>();
+
+            if (health != null && health.ReduceHealth(damage))
+            {
+
+                Destroy(health.gameObject);
+
+            }
+            else
+            {
+
+                transform.up = Vector3.Reflect(transform.up, rayHit.normal);
+
+                numberOfHits--;
+
+                if (numberOfHits <= 0)
+                {
+
+                    Destroy(gameObject);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
